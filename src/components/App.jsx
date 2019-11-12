8ccdf6a000094a0e87592a1491752076ac92d7c6
Lauren <Lauren.Musante@gmail.com>
2019-11-11 16:50:52 -0800

    Added lates version of Webpack - 4.41.2. Got rid of some warnings, but not all.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,6 @@ function App(){
 export default App;
 
 
-export default App;
+// export default App;
 
 //, we can only render a single React element at a time, but that element may contain limitless children. So anytime we want to render multiple elements we need to enclose them in a single parent,
